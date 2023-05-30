02ccbf16472417771d68bd3edc2f7159e1cb1807
t0pall <i@itopal.ru>
2023-05-30 17:27:45 +0300

    Добавить асинхронную загрузку компонентов и роутинг

--- src/app.tsx
+++ src/app.tsx
@@ -1,11 +1,20 @@
-import { Counter } from "./components/counter";
-import './index.scss'
+import { Link, Route, Routes } from "react-router-dom";
+import { Suspense } from "react";
+import "./index.scss";
+import { MainPageAsync } from "./pages/mainPage/mainPage.async";
+import { AboutPageAsync } from "./pages/aboutPage/aboutPage.async";
 
 const App = () => {
   return (
     <div className="app">
-      sdfsdfsdf
-      <Counter />
+      <Link to="/">Main</Link>
+      <Link to="/about">About</Link>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path="/" element={<MainPageAsync />} />
+          <Route path="/about" element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
     </div>
   );
 };
---
+++ src/pages/aboutPage/aboutPage.tsx
@@ -0,0 +1,5 @@
+const AboutPage = () => {
+  return ( <div>ABOUT</div> );
+}
+ 
+export default AboutPage;
