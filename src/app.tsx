6a2a4a0df8203db1015679a97a413420b788de9f
t0pall <i@itopal.ru>
2023-06-02 18:03:02 +0300

    Добавить поддержку тем

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,18 @@
 import { Link, Route, Routes } from "react-router-dom";
 import { Suspense } from "react";
-import "./index.scss";
 import { MainPageAsync } from "./pages/mainPage/mainPage.async";
 import { AboutPageAsync } from "./pages/aboutPage/aboutPage.async";
+import "./styles/index.scss";
+import { useTheme } from "./theme/useTheme";
 
 const App = () => {
+  const {theme, toggleTheme} = useTheme();
+
   return (
-    <div className="app">
+    <div className={`app ${theme}`}>
       <Link to="/">Main</Link>
       <Link to="/about">About</Link>
+      <button onClick={toggleTheme}>Сменить тему</button>
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route path="/" element={<MainPageAsync />} />
