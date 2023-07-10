aa94982f0e55eb2023e1d2955af1a6dccabdfbf2
Igor Topal <i@itopal.ru>
2023-07-10 16:03:35 +0300

    feat: add babel, add extract plugin

--- config/build/buildLoaders.ts
+++ config/build/buildLoaders.ts
@@ -23,6 +23,23 @@ export function buildLoaders(options: BuildOptions): RuleSetRule[] {
     use: ['@svgr/webpack'],
   };
 
+  const babelLoader = {
+    test: /\.(js|ts|jsx|tsx)$/,
+    exclude: /node_modules/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+        plugins: [
+          [
+            'i18next-extract',
+            { locales: ['ru', 'en'], keyAsDefaultValue: true },
+          ],
+        ],
+      },
+    },
+  };
+
   const scssLoader = {
     test: /\.s[ac]ss$/i,
     use: [
@@ -42,5 +59,5 @@ export function buildLoaders(options: BuildOptions): RuleSetRule[] {
     ],
   };
 
-  return [typescryptLoader, svgLoader, scssLoader, fileLoader];
+  return [fileLoader, svgLoader, babelLoader, typescryptLoader, scssLoader];
 }
