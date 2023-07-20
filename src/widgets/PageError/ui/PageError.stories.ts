20f27ddf20c29c231d5f6212afaf5e3d657017b4
Igor Topal <i@itopal.ru>
2023-07-20 14:18:40 +0300

    add loki

--- src/widgets/PageError/ui/PageError.stories.ts
+++ src/widgets/PageError/ui/PageError.stories.ts
@@ -1,6 +1,4 @@
 import { Meta, StoryObj } from '@storybook/react';
-import themeDecorator from 'shared/config/storybook/decorators/themeDecorator';
-import { Theme } from 'app/providers/themeProvider';
 import PageError from './PageError';
 
 const meta = {
@@ -14,7 +12,3 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Primary: Story = {};
-
-export const Dark: Story = {
-  decorators: [themeDecorator(Theme.DARK)],
-};
