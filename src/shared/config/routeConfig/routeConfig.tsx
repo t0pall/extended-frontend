4aff19f1f5f933e522b9998438f8f563ef45d90c
Igor Topal <i@itopal.ru>
2023-07-20 16:02:53 +0300

    Update routeConfig.tsx

--- src/shared/config/routeConfig/routeConfig.tsx
+++ src/shared/config/routeConfig/routeConfig.tsx
@@ -1,6 +1,6 @@
-import { AboutPage } from 'pages/aboutPage';
+import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
-import { NotFoundPage } from 'pages/notFoundPage';
+import { NotFoundPage } from 'pages/NotFoundPage';
 import { RouteProps } from 'react-router-dom';
 
 export enum AppRoutes {
