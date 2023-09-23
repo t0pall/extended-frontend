6d5256555b59bae55e4ffed72d7b7bbb34cee8ee
Igor Topal <i@itopal.ru>
2023-09-23 10:49:10 +0300

    add stories for sidebar, fix loki screens

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.ts
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.stories.ts
@@ -1,6 +1,7 @@
 import { Meta, StoryObj } from '@storybook/react';
 import themeDecorator from 'shared/config/storybook/decorators/themeDecorator/themeDecorator';
 import { Theme } from 'app/providers/themeProvider';
+import StateDecorator from 'shared/config/storybook/decorators/StateDecorator';
 import Sidebar from './Sidebar';
 
 const meta = {
@@ -13,8 +14,14 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+  decorators: [StateDecorator({ user: { authData: {} } })],
+};
 
 export const Dark: Story = {
-  decorators: [themeDecorator(Theme.DARK)],
+  decorators: [themeDecorator(Theme.DARK), StateDecorator({ user: { authData: {} } })],
+};
+
+export const NotAuth: Story = {
+  decorators: [StateDecorator({ user: {} })],
 };
